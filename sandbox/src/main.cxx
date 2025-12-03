7d24d1807c097ba48be2c37be07ecadd7b1974a0
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2025-12-03 22:30:37 +0600

    refactor: Add sandbox test case for detecting segfault

--- sandbox/src/main.cxx
+++ sandbox/src/main.cxx
@@ -7,6 +7,14 @@ private:
   RipsawEngine::Actor* a1{nullptr};
   RipsawEngine::TransformComponent* tc1{nullptr};
   RipsawEngine::SpriteComponent* sc1{nullptr};
+  
+  RipsawEngine::Actor* a2{nullptr};
+  RipsawEngine::TransformComponent* tc2{nullptr};
+  RipsawEngine::SpriteComponent* sc2{nullptr};
+  
+  RipsawEngine::Actor* a3{nullptr};
+  RipsawEngine::TransformComponent* tc3{nullptr};
+  RipsawEngine::SpriteComponent* sc3{nullptr};
 
 public:
   void initGame() override
@@ -14,6 +22,14 @@ public:
     a1 = new RipsawEngine::Actor{mEngine};
     tc1 = new RipsawEngine::TransformComponent(a1, {1300, 400}, {-250, 0});
     sc1 = new RipsawEngine::SpriteComponent(a1, mEngine->getRenderer(), {30, 30}, {0, 255, 255, 255});
+    
+    a2 = new RipsawEngine::Actor{mEngine};
+    tc2 = new RipsawEngine::TransformComponent(a2, {1300, 300}, {-250, 0});
+    sc2 = new RipsawEngine::SpriteComponent(a2, mEngine->getRenderer(), {30, 30}, {255, 0, 0, 255});
+    
+    a3 = new RipsawEngine::Actor{mEngine};
+    tc3 = new RipsawEngine::TransformComponent(a3, {1300, 200}, {-250, 0});
+    sc3 = new RipsawEngine::SpriteComponent(a3, mEngine->getRenderer(), {30, 30}, {0, 255, 0, 255});
   }
 
   void updateGame(double dt) override
@@ -21,6 +37,14 @@ public:
     if (a1->getPosition().x <= 700)
     {
       a1->setPosition({1300, 400});
+    }
+    if (a2->getPosition().x <= 700)
+    {
+      a2->setPosition({1300, 300});
+    }
+    if (a3->getPosition().x <= 700)
+    {
+      a3->setPosition({1300, 200});
     }
   }
 
